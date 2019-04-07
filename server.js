d451536e88a0808df13171f7ec73bbede050ad95
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-06 19:00:42 -0700

    Create basic routes

--- server.js
+++ server.js
@@ -12,8 +12,38 @@ const app = express();
 app.use(morgan('dev'));
 
 /*
+  Simple 'DB'
+*/
+const users = {};
+
+/*
   Routes
 */
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(users));
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}!`))
+app.get('/user', (req, res) => {
+  const { userId } = req.query;
+  res.send(users[userId]);
+});
+
+app.post('/bpm', (req, res) => {
+  const { userId, bpm } = req.query;
+
+  if (!users[userId]) users[userId] = {};
+
+  users[userId].bpm = bpm;
+
+  res.sendStatus(200);
+});
+
+app.post('/location', (req, res) => {
+  const { userId, lat, lon } = req.query;
+
+  if (!users[userId]) users[userId] = {};
+
+  users[userId].location = { lat, lon };
+
+  res.sendStatus(200);
+});
+
+app.listen(config.port, () => console.log(`Server running on port ${config.port}!`));
